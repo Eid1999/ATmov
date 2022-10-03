3c04777e4a79341c58bd0a098eda1d83f4c42cdb
Eid <pedrohenrique_eid@hotmail.com>
2022-10-03 19:10:20 +0100

    Sensores a funcionar

--- app/src/main/java/com/example/pat_1/SensorActivity.java
+++ app/src/main/java/com/example/pat_1/SensorActivity.java
@@ -1,11 +1,9 @@
 package com.example.pat_1;
 
-import static android.app.PendingIntent.getActivity;
 
 import android.app.Activity;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.content.Intent;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -17,9 +15,7 @@ import android.widget.Toast;
 public class SensorActivity extends Activity implements SensorEventListener {
     private SensorManager sensorManager;
     public Sensor envSense,temp,light, hum;
-    private TextView ambientValue, lightValue, humidityValue;
-    private final int AMBIENT=0;
-    private TextView[] valueFields = new TextView[4];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,69 +25,57 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
         // Get an instance of the sensor service, and use that to get an instance of
         // a particular sensor.
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         temp = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
         light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         hum = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+
+
+    }
+    protected void onResume() {
+        super.onResume();
+
         sensorManager.registerListener(this, temp, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this, hum, SensorManager.SENSOR_DELAY_NORMAL);
 
 
 
-        /*light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if(light==null)
-            Toast.makeText(this.getApplicationContext(),
-                    "Sorry - your device doesn't have an ambient light sensor!",
-                    Toast.LENGTH_SHORT).show();
-        else
-            sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
 
-
-        hum = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
-        if(hum==null)
-            Toast.makeText(this.getApplicationContext(),
-                    "Sorry - your device doesn't have an ambient temperature sensor!",
-                    Toast.LENGTH_SHORT).show();
-        else
-            sensorManager.registerListener(this, hum, SensorManager.SENSOR_DELAY_NORMAL);*/
 
     }
+
+
     public final void onSensorChanged(SensorEvent event) {
         float sensorValue = event.values[0];
 
 
         // Do something with this sensor data.
-        TextView currValue = ambientValue;
-        String envInfo="";
+        String envInfo;
         int currType=event.sensor.getType();
 
 
 
         switch(currType){
             case Sensor.TYPE_AMBIENT_TEMPERATURE:
-                envInfo=sensorValue+" degrees Celsius";
-                setContentView(R.layout.activity_sensor);
-                TextView txtView = (TextView) findViewById(R.id.txtTEMP);
+                envInfo=sensorValue+" Celsius";
+
+                TextView txtView = findViewById(R.id.txtTEMP);
                 txtView.setText(" "+envInfo);
                 break;
-            /*case Sensor.TYPE_LIGHT:
-                envInfo=sensorValue+" SI lux units";
-                setContentView(R.layout.activity_sensor);
-                TextView txtView2 = (TextView) findViewById(R.id.txtLIGHT);
+            case Sensor.TYPE_LIGHT:
+                envInfo=sensorValue+" lm";
+                TextView txtView2 = findViewById(R.id.txtLIGHT);
                 txtView2.setText(" "+envInfo);
                 break;
             case Sensor.TYPE_RELATIVE_HUMIDITY:
                 envInfo=sensorValue+" percent humidity";
-                setContentView(R.layout.activity_sensor);
-                TextView txtView3 = (TextView) findViewById(R.id.txtHUM);
+                TextView txtView3 = findViewById(R.id.txtHUM);
                 txtView3.setText(" "+envInfo);
                 break;
-            default: break;*/
+            default: break;
         }
-
-        envSense=null;
-        sensorManager.unregisterListener(this);
     }
 
     @Override
