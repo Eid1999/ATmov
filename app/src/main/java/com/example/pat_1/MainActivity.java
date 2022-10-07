34e7040db71010e4c850ddd35d6078598fdde944
Eid1999 <pedrohenrique_eid@hotmail.com>
2022-10-07 12:50:58 +0100

    Sensores na interface

--- app/src/main/java/com/example/pat_1/MainActivity.java
+++ app/src/main/java/com/example/pat_1/MainActivity.java
@@ -25,9 +25,11 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
-    private SensorManager senseManage;
-    private Sensor envSense;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    public Sensor envSense,temp,light, hum;
+    public String Slight,Shum,Stemp;
+
 
     //public ArrayList<Float> temp_repo = new ArrayList<Float>();
     //public ArrayList<Float> light_repo = new ArrayList<Float>();
@@ -41,11 +43,17 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
-        senseManage = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        temp = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        hum = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+
     }
     /** Called when the user taps the Show Sensors button */
     public void show_sensors(View view) {
         Intent intent = new Intent(this, SensorActivity.class);
+
         startActivity(intent);
     }
 
@@ -53,6 +61,88 @@ public class MainActivity extends AppCompatActivity {
     public void change_threshold(View view) {
         Intent intent = new Intent(this, threshold_alarm.class);
         startActivity(intent);
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        sensorManager.registerListener(this, temp, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, hum, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+
+
+
+    }
+
+
+    public final void onSensorChanged(SensorEvent event) {
+        float sensorValue = event.values[0];
+
+
+        // Do something with this sensor data.
+        String envInfo;
+        int currType=event.sensor.getType();
+
+        switch(currType){
+            case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                Stemp=sensorValue+" Celsius";
+                //if (current time += 1 minute) <= repository[0][0]{
+                //      temp_repo.remove(9);
+                //      temp_repo.add(envInfo)
+                //}
+                break;
+
+            case Sensor.TYPE_LIGHT:
+                Slight=sensorValue+" lm";
+
+                //if (current time += 1 minute) <= repository[0][0]{
+                //      light_repo.remove(9);
+                //      light_repo.add(envInfo)
+                //}
+
+                break;
+            case Sensor.TYPE_RELATIVE_HUMIDITY:
+                Shum=sensorValue+" percent humidity";
+
+                break;
+            default: break;
+
+        }
+
+
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        String accuracyMsg = "";
+        switch(accuracy){
+            case SensorManager.SENSOR_STATUS_ACCURACY_HIGH:
+                accuracyMsg="Sensor has high accuracy";
+                break;
+            case SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM:
+                accuracyMsg="Sensor has medium accuracy";
+                break;
+            case SensorManager.SENSOR_STATUS_ACCURACY_LOW:
+                accuracyMsg="Sensor has low accuracy";
+                break;
+            case SensorManager.SENSOR_STATUS_UNRELIABLE:
+                accuracyMsg="Sensor has unreliable accuracy";
+                break;
+            default:
+                break;
+        }
+        Toast accuracyToast = Toast.makeText(this.getApplicationContext(), accuracyMsg, Toast.LENGTH_SHORT);
+        accuracyToast.show();
+    }
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
 
